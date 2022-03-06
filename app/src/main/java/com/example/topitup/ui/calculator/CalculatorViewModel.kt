5b509e159eaadc38f80ba38c1a3e794d433b0d3a
Mohammed Chotia <mc@kutimo.co.za>
2022-03-06 16:45:59 +0200

    Initial Commit - Scaffold

---
+++ app/src/main/java/com/example/topitup/ui/calculator/CalculatorViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.topitup.ui.calculator
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CalculatorViewModel: ViewModel() {
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is calculator Fragment"
+    }
+    val text: LiveData<String> = _text
+}
