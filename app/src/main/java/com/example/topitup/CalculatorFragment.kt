6efd66da2d8562b87993dc0e5df8ff3dacab064e
Mohammed Chotia <mc@kutimo.co.za>
2022-03-13 20:29:05 +0200

    Make calculator work as expected.

--- app/src/main/java/com/example/topitup/CalculatorFragment.kt
+++ app/src/main/java/com/example/topitup/CalculatorFragment.kt
@@ -1,20 +1,26 @@
 package com.example.topitup
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.topitup.databinding.CalculatorFragmentBinding
 import com.example.topitup.viewmodels.CalculatorViewModel
+import java.lang.Exception
+import java.text.DecimalFormat
 
 class CalculatorFragment : Fragment() {
 
     private var _binding: CalculatorFragmentBinding? = null
 
-    private var screenValue: Long = 0
-    private var previousValue: Long = 0
+    private var firstValue: Double = Double.NaN
+    private var secondValue: Double = 0.0
     private var operator: String = ""
-    var screenText: String = ""
+    private val formatDecimal = DecimalFormat("#.##########")
+    private var screenText: String = ""
+    private var isDot: Boolean = false
+    private var clearScreen: Boolean = false
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,8 +31,8 @@ class CalculatorFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val calculatorViewModel =
-            ViewModelProvider(this).get(CalculatorViewModel::class.java)
+        /* val calculatorViewModel =
+             ViewModelProvider(this).get(CalculatorViewModel::class.java)*/
 
         _binding = CalculatorFragmentBinding.inflate(inflater, container, false)
         val root: View = binding.root
@@ -34,154 +40,6 @@ class CalculatorFragment : Fragment() {
         setHasOptionsMenu(true)
         return root
     }
-
-/*
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        ////////////////////////////////////////////////////////////////////////////////////
-        var isNewOp=true
-        var dot=false
-        val etShowNumber = binding.editTextNumberDecimal
-        fun buNumberEvent(view: View)
-        {
-            if(isNewOp)
-            {
-                etShowNumber.setText("")
-            }
-            isNewOp=false
-            val buSelect= view as Button
-            var buClickValue:String=etShowNumber.text.toString()
-            when(buSelect.id)
-            {
-                button0.id->
-                {
-                    buClickValue+="0"
-                }
-                button1.id->
-                {
-                    buClickValue+="1"
-                }
-                button2.id->
-                {
-                    buClickValue+="2"
-                }
-                button3.id->
-                {
-                    buClickValue+="3"
-                }
-                button4.id->
-                {
-                    buClickValue+="4"
-                }
-                button5.id->
-                {
-                    buClickValue+="5"
-                }
-                button6.id->
-                {
-                    buClickValue+="6"
-                }
-                button7.id->
-                {
-                    buClickValue+="7"
-                }
-                button8.id->
-                {
-                    buClickValue+="8"
-                }
-                button9.id->
-                {
-                    buClickValue+="9"
-                }
-                buttonDot.id->
-                {
-                    if(dot==false)
-                    {
-                        buClickValue += "."
-                    }
-                    dot=true
-                }
-                buttonPlusMinus.id->
-                {
-                    buClickValue="-" + buClickValue
-                }
-            }
-            etShowNumber.setText(buClickValue)
-        }
-        var op="X"
-        var oldNumber=""
-
-        fun buOpEvent(view: View)
-        {
-            val buSelect= view as Button
-            when(buSelect.id)
-            {
-                buttonMuliply.id->
-                {
-                    op="X"
-                }
-                buttonDivide.id->
-                {
-                    op="÷"
-                }
-                buttonSubtract.id->
-                {
-                    op="-"
-                }
-                buttonPlus.id->
-                {
-                    op="+"
-                }
-            }
-            oldNumber=etShowNumber.text.toString()
-            isNewOp=true
-            dot=false
-        }
-
-        fun buEqualEvent(view: View)
-        {
-            val newNumber=etShowNumber.text.toString()
-            var finalNumber:Double?=null
-            when(op)
-            {
-                "X"->
-                {
-                    finalNumber=oldNumber.toDouble() * newNumber.toDouble()
-                }
-                "÷"->
-                {
-                    finalNumber=oldNumber.toDouble() / newNumber.toDouble()
-                }
-                "-"->
-                {
-                    finalNumber=oldNumber.toDouble() - newNumber.toDouble()
-                }
-                "+"->
-                {
-                    finalNumber=oldNumber.toDouble() + newNumber.toDouble()
-                }
-            }
-            etShowNumber.setText(finalNumber.toString())
-            isNewOp=true
-        }
-
-        fun buPercentEvent(view: View)
-        {
-            val number=(etShowNumber.text.toString().toDouble())/100
-            etShowNumber.setText(number.toString())
-            isNewOp=true
-        }
-
-        fun buCleanEvent(view: View)
-        {
-            etShowNumber.setText("")
-            isNewOp=true
-            dot=false
-        }
-
-        ////////////////////////////////////////////////////////////////////////////////////
-    }
-    */
 
     override fun onDestroyView() {
         super.onDestroyView()
@@ -196,7 +54,6 @@ class CalculatorFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
         binding.button0.setOnClickListener(listener)
         binding.button1.setOnClickListener(listener)
         binding.button2.setOnClickListener(listener)
@@ -209,7 +66,7 @@ class CalculatorFragment : Fragment() {
         binding.button9.setOnClickListener(listener)
         binding.buttonAC.setOnClickListener(listener)
         binding.buttonPlusMinus.setOnClickListener(listener)
-        binding.buttonPerc.setOnClickListener(listener)
+        binding.buttonPercentage.setOnClickListener(listener)
         binding.buttonDivide.setOnClickListener(listener)
         binding.buttonMultiply.setOnClickListener(listener)
         binding.buttonMinus.setOnClickListener(listener)
@@ -217,10 +74,35 @@ class CalculatorFragment : Fragment() {
         binding.buttonDot.setOnClickListener(listener)
         binding.buttonEqual.setOnClickListener(listener)
 
-
     }
-    val listener = View.OnClickListener { view ->
-        when (view.getId()) {
+
+    private fun calculate() {
+        if (!firstValue.isNaN() and !screenText.isNullOrEmpty()) {
+            secondValue = screenText.toDouble()
+            binding.editTextNumberDecimal.text = null
+            when (operator) {
+                "+" -> firstValue += secondValue
+                "-" -> firstValue -= secondValue
+                "*" -> firstValue *= secondValue
+                "/" -> firstValue /= secondValue
+            }
+            screenText = formatDecimal.format(firstValue).toString()
+        } else {
+            try {
+                firstValue = screenText.toDouble()
+            } catch (e: Exception) {
+                Log.d("Calculator", e.toString())
+            }
+        }
+        isDot = false
+    }
+
+    private val listener = View.OnClickListener { view ->
+        if (clearScreen) {
+            screenText = ""
+            clearScreen = false
+        }
+        when (view.id) {
             R.id.button1 -> {
                 screenText += "1"
             }
@@ -252,47 +134,62 @@ class CalculatorFragment : Fragment() {
                 screenText += "0"
             }
             R.id.buttonDot -> {
+                if (!isDot) {
+                    screenText += "."
+                }
+                isDot = true
             }
             R.id.buttonPlus -> {   //"＋"
+                calculate()
                 operator = "+"
-                screenValue = screenText.toLong()
-                //TODO: Add boolean to update number
-                screenText = ""
+                clearScreen = true
+
             }
             R.id.buttonMinus -> {
-                operator =  "-"
+                calculate()
+                operator = "-"
+                clearScreen = true
             }
             R.id.buttonMultiply -> {
-                operator =  "x"
+                calculate()
+                operator = "*"
+                clearScreen = true
             }
             R.id.buttonDivide -> {
-                operator =  "/"
+                calculate()
+                operator = "/"
+                clearScreen = true
             }
-            R.id.buttonPerc -> {
+            R.id.buttonPercentage -> {
                 // "％"
+                if (!screenText.isNullOrEmpty()) {
+                    screenText = formatDecimal.format((screenText.toDouble() / 100)).toString()
+                }
             }
             R.id.buttonPlusMinus -> {
                 // "+/-"
+                if (!screenText.isNullOrEmpty()) {
+                    screenText = formatDecimal.format((screenText.toDouble() * -1)).toString()
+                }
             }
             R.id.buttonEqual -> {
                 // "="
-                when (operator) {
-                    "+" -> {screenText = (screenValue + previousValue).toString()}
-                    "-" -> {screenText = (screenValue - previousValue).toString()}
-                    "x" -> {screenText = (screenValue * previousValue).toString()}
-                    "/" -> {screenText = (screenValue / previousValue).toString()}
-                }
+                calculate()
+                screenText = formatDecimal.format(firstValue).toString()
+                firstValue = Double.NaN
+                clearScreen = false
+
             }
             R.id.buttonAC -> {
                 screenText = ""
-                screenValue = 0
-                previousValue = 0
+                secondValue = Double.NaN
+                firstValue = Double.NaN
+                isDot = false
             }
         }
+
         binding.editTextNumberDecimal.text = screenText
     }
-
-
 
 
 }
